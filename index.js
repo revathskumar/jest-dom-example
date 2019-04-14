8aed25e76cb1c12e6fc0cb8f89a9e71d484d1a17
Revath S Kumar <rsk@revathskumar.com>
2019-04-14 19:04:52 +0530

    add tests to test asymmetric matchers
    for `toHaveAttribute` value.
    
    gnapse/jest-dom#91

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <a href="">Link</a>
+      <a href="/some_url?type=ab">Link</a>
+      <a href="/some_url?type=abc">Link 1</a>
+      <a href="/some_url?type=abcd">Link 2</a>
     </div>
   );
 };
--- test.js
+++ test.js
@@ -8,6 +8,14 @@ import App from "./";
 describe("App", () => {
   it("have attribute", () => {
     const { container, getByText } = render(<App />);
-    expect(getByText("Link")).toHaveAttribute("href", "");
+    expect(getByText("Link 1")).toHaveAttribute(
+      "href",
+      expect.stringContaining("abc"),
+    );
+    expect(getByText("Link")).toHaveAttribute(
+      "href",
+      expect.stringMatching(/type=ab/),
+    );
+    expect(getByText("Link 2")).toHaveAttribute("href", expect.anything());
   });
 });
